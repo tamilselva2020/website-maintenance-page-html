748365c2e68041185dcbcd576761db52605f27af
tamilselva2020 <tamilselva2015@gmail.com>
2024-09-06 22:55:14 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-<title> %customer_name% - Site Maintenance</title>
+<title> <?php echo getenv('customer_name'); ?> - Site Maintenance</title>
 <meta charset="utf-8"/>
 <meta name="robots" content="noindex"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
